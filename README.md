d076b41dbefd30f5d64befcbfac141ba250736a1
sirgeo <sirgeo111@yahoo.com>
2022-12-08 15:54:37 -0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# MLT_aerials
-MLT's 1998-2021 Aerials per King County
+# City of MLT Aerial vintages
+mouseover js aerials
---
+++ js_mouseover.js
@@ -0,0 +1,15 @@
+<li onmouseover="changeImage("https://imgur.com/mOlnero")"> 1880to1884</li> 
+<script type="text/javascript"> 
+function changeImage(source) { 
+document.getElementById('https://imgur.com/mOlnero').src=source; 
+} 
+</script> 
+
+<li onmouseover="changeImage("https://i.imgur.com/ahUU64P.jpg")"> 1998</li> 
+<li onmouseover="changeImage("https://i.imgur.com/4o3k37x.jpg")"> 2000</li> 
+<li onmouseover="changeImage("https://i.imgur.com/74NB6Hn.jpg")"> 2005</li> 
+<li onmouseover="changeImage("https://i.imgur.com/rCaCehj.jpg")"> 2009</li> 
+<li onmouseover="changeImage("https://i.postimg.cc/6t9SGbfB/2015.png")"> 2015</li> 
+<li onmouseover="changeImage("https://i.postimg.cc/tR9rW7PB/2019.png")"> 2019</li> 
+<li onmouseover="changeImage("https://i.postimg.cc/pR4ftpT3/2021.png")"> 2021</li> 
+
